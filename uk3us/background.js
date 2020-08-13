307477a43647e49ec43724fcc48e12f94c8df858
Dafydd Roberts <nubotix@gmail.com>
2020-08-13 15:30:31 +0100

    cadarn a morfil

---
+++ uk3us/background.js
@@ -0,0 +1,18 @@
+// Examples The following code creates an IME that converts typed letters to upper case.
+var context_id = -1;
+
+chrome.input.ime.onFocus.addListener(function(context) {
+  context_id = context.contextID;
+});
+
+chrome.input.ime.onKeyEvent.addListener(
+  function(engineID, keyData) {
+    if (keyData.type == "keydown" && keyData.key.match(/^[aeiouwy]$/)) {
+      whale.input.ime.commitText({"contextID": context_id,
+                                   "text": keyData.key.toUpperCase()});
+      return true;
+    } else {
+      return false;
+    }
+  });
+
