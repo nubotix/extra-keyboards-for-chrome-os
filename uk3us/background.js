6c041212f0639f50793e611cbe46567403bdf3de
Dafydd Roberts <nubotix@gmail.com>
2020-08-13 15:34:21 +0100

    morfil

--- uk3us/background.js
+++ uk3us/background.js
@@ -8,7 +8,7 @@ chrome.input.ime.onFocus.addListener(function(context) {
 chrome.input.ime.onKeyEvent.addListener(
   function(engineID, keyData) {
     if (keyData.type == "keydown" && keyData.key.match(/^[aeiouwy]$/)) {
-      whale.input.ime.commitText({"contextID": context_id,
+      chrome.input.ime.commitText({"contextID": context_id,
                                    "text": keyData.key.toUpperCase()});
       return true;
     } else {
