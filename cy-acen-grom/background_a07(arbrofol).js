f5ac82ee963c6b258b257a1d9b27cc6517c8d42c
Dafydd Roberts <nubotix@gmail.com>
2020-08-19 12:20:53 +0100

    ailenwi

--- cy-acen-grom/background_a07(arbrofol).js
+++ cy-acen-grom/background_a07(arbrofol).js
@@ -80,17 +80,16 @@ chrome.input.ime.onKeyEvent.addListener(
           handled = true;
           //AltGr = false;
         } 
-        return handled;    
-});
-chrome.input.ime.onKeyEvent.addListener(
-  function(engineID, keyData) {
-    var handled = false;
+       //  
+
+    
 
     updateAltGrState(keyData);
 
-    if (AltGr && keyData.type == "up" )/*&& !isPureModifier(keyData)*/
+    if (AltGr && keyData.type == "keyup" ){/*&& !isPureModifier(keyData)*/
       
         AltGr = false;
+    }
         return handled;
 }
 
