b0d3f72c28f9205c9328e8bda2688eb893bfde6e
Dafydd Roberts <nubotix@gmail.com>
2020-08-20 13:25:07 +0100

    man newidiadau

--- uk3us/background.js
+++ uk3us/background.js
@@ -1,18 +1,54 @@
 // An IME that converts typed Welsh vowels to upper case.
-var context_id = -1;
+var previousCharIsMagic = false;
+var contextID = -1;
+const circumflexed = {
+  "a": "\u00e2", 
+  "A": "\u00c2", 
+  "e": "\u00ea", 
+  "E": "\u00ca", 
+  "i": "\u00ee", 
+  "I": "\u00ce", 
+  "o": "\u00f4", 
+  "O": "\u00d4", 
+  "u": "\u00fb", 
+  "U": "\u00db", 
+  "w": "\u0175", 
+  "W": "\u0174", 
+  "y": "\u0177", 
+  "Y": "\u0176", 
+  /*"`": "\u2018"*/
+};
 
 chrome.input.ime.onFocus.addListener(function(context) {
-  context_id = context.contextID;
+  contextID = context.contextID;
 });
 
-chrome.input.ime.onKeyEvent.addListener(
-  function(engineID, keyData) {
-    if (/*KeyboardEvent.altgrKey && */ keyData.type == "keydown" && keyData.key.match(/^[aeiouwy]$/)) {
-      chrome.input.ime.commitText({"contextID": context_id,
-                                   "text": keyData.key.toUpperCase()});
-      return true;
-    } else {
-      return false;
-    }
-  });
+function isPureModifier(keyData) {
+  return (keyData.key == "Shift") || (keyData.key == "Ctrl") || (keyData.key == "Alt");
+  //return (keyData.key == "AltGraph")|| (keyData.key == "AltRight");
+}
 
+chrome.input.ime.onKeyEvent.addListener(
+    function(engineID, keyData) {
+      var handled = false;
+      
+      if (previousCharIsMagic && keyData.type == "keydown" && !isPureModifier(keyData)) {
+        previousCharIsMagic = false;
+        if (circumflexed[keyData.key]) {
+          chrome.input.ime.commitText({"contextID": contextID,
+                                   "text": circumflexed[keyData.key]});
+          handled = true;
+        } 
+       // else {
+       //   chrome.input.ime.commitText({"contextID": contextID,
+       //                           "text": "`"});
+       //}
+      }
+      
+      if (!handled && keyData.type == "keydown" && keyData.code == "AltRight" /*&& keyData.key ==""*/) {
+        previousCharIsMagic = true;
+        handled = true;
+      }
+      
+      return handled;
+});
